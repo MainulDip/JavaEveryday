55559ea8240941f4736fd01c4ab01be8f4e3db5d
mainuldip <mainuldip@gmail.com>
2022-02-03 22:11:37 -0500

    agendas updating

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+//    variables
+
+//    types
+
+//    privacy
+
+//    final
+
+//    function declarations
+
+//    void
+
+//    main function
+
+//    classes and instances
+
+//    constructors
+
+//    toString
+
+//    static
+}
