a3c1816ee61a71018f97e8e45cd8f22c63ece0ff
mainuldip <mainuldip@gmail.com>
2022-03-04 02:12:36 -0500

    java interactivity docking success

--- HelloWorld/app/src/main/java/com/websolverpro/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/websolverpro/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.websolverpro.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        btnClicking();
+    }
+
+    public void btnClicking(View view) {
+        TextView txtHello = findViewById(R.id.idHelloWorldText);
+        txtHello.setText("Java Calling");
     }
 }
