f57914ad5a62a2b677c72283c30fb2af5ebbf80f
mainuldip <mainuldip@gmail.com>
2022-03-05 02:13:02 -0500

    data handeling docking

--- HelloWorld/app/src/main/java/com/websolverpro/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/websolverpro/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +14,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        btnClicking();
+
     }
+
+    private Boolean counter = false;
+    private CharSequence oldText;
 
     public void btnClicking(View view) {
         TextView txtHello = findViewById(R.id.idHelloWorldText);
-        txtHello.setText("Java Calling");
+        CharSequence newText = "Java Calling";
+
+        EditText editTextName = findViewById(R.id.editTextName);
+
+
+        txtHello.setText(editTextName.getText().toString());
+        editTextName.setText("");
+        editTextName.clearFocus();
+
+//        if (!counter){
+//            this.oldText = txtHello.getText();
+//            txtHello.setText(newText);
+//            this.counter = true;
+//            System.out.println("Counter Initialized");
+//            return;
+//        }
+//
+//        if(txtHello.getText() == oldText){
+//            txtHello.setText(newText);
+//            System.out.println("Text View Value Changed to : " + txtHello.getText());
+//        } else if (txtHello.getText() == newText){
+//            txtHello.setText(oldText);
+//            System.out.println("Text View Value Changed to : " + txtHello.getText());
+//        }
     }
 }
+
+// Code is Poetry :)
