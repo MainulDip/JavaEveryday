883cf00cfaab2d739dde19691d8e23a3d85ff2a7
mainuldip <mainuldip@gmail.com>
2022-03-08 23:20:28 -0500

    Java Singleton Pattern Docking

---
+++ SingletonDatabase/src/com/websolverpro/singletondatabase/SingletonDatabase.java
@@ -0,0 +1,26 @@
+package com.websolverpro.singletondatabase;
+
+import com.websolverpro.singletondatabase.database.Database;
+
+/**
+ * Singleton Pattern
+ * Force using only one instance and stop creating multiple instance
+ * Force thread safe.
+ * Tracks single instance through static prop and method, as static props and methods belongs to that class inseft, not to the instances
+ * As static props and methods belongs to that object/class itself, it can be used as a counter.
+ * we instantiate the object by calling the static method and keep tracks of the instance number through static property by checking instance == null
+ * Also restrict calling of the constructor from outside the class using encapsulation (private)
+ * Also make getInstance method thread safe using 'synchronized' keyword, it restricts multiple thread to call this method.
+ * If multiple thread call this method at the same time, there might be some chance of getting multiple instance created.
+ */
+
+public class SingletonDatabase {
+    public static void main(String[] args) {
+        System.out.println("Starting Singleton Pattern Database");
+        Database database = Database.getInstance("singleton-database");
+        Database anotherDatabase = Database.getInstance("Second Database");
+
+        System.out.println("First Database Name: " + database.getName()); // First Database Name: singleton-database
+        System.out.println("Second Database Name: " + anotherDatabase.getName()); // First Database Name: singleton-database
+    }
+}
---
+++ SingletonDatabase/src/com/websolverpro/singletondatabase/database/Database.java
@@ -0,0 +1,41 @@
+package com.websolverpro.singletondatabase.database;
+
+/**
+ * Singleton Pattern
+ * Force using only one instance and stop creating multiple instance
+ * Force thread safe.
+ * Tracks single instance through static prop and method, as static props and methods belongs to that class inseft, not to the instances
+ * As static props and methods belongs to that object/class itself, it can be used as a counter.
+ * we instantiate the object by calling the static method and keep tracks of the instance number through static property by checking instance == null
+ * Also restrict calling of the constructor from outside the class using encapsulation (private)
+ * Also make getInstance method thread safe using 'synchronized' keyword, it restricts multiple thread to call this method.
+ * If multiple thread call this method at the same time, there might be some chance of getting multiple instance created.
+ */
+
+public class Database {
+    public String name;
+
+    public static Database instance;
+
+//    to force stop using multiple threads so there will be no multiple instance errors.
+    public static synchronized Database getInstance(String name) {
+        if( instance == null ) {
+            instance = new Database(name);
+        }
+        System.out.println(instance.name);
+        return instance;
+    }
+
+//    make constructor private to restrict access from outside to create instance.
+    private Database(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
