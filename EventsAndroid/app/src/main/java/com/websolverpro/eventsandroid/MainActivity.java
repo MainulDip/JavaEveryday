509caf64ab1e7196229e3564f35d3ce32be3831e
mainuldip <mainuldip@gmail.com>
2022-03-12 23:56:09 -0500

    playing with checkbox radio

--- EventsAndroid/app/src/main/java/com/websolverpro/eventsandroid/MainActivity.java
+++ EventsAndroid/app/src/main/java/com/websolverpro/eventsandroid/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +37,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return false;
             }
         });
+
+//        playing with checkbox
+
+        CheckBox firstLeftCB = findViewById(R.id.leftFirstCheckBox);
+        firstLeftCB.setOnClickListener(this);
+
+        if (firstLeftCB.isChecked()){
+            System.out.println("Harry potter is selected");
+        }
+
+        firstLeftCB.setChecked(false);
+        firstLeftCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "Checkbox checking/unchecking", Toast.LENGTH_SHORT).show();
+                    System.out.println("Checkbox Checked Harry Potter");
+                } else {
+                    System.out.println("UN Checked Harry Potter");
+                }
+
+            }
+        });
     }
 
     @Override
@@ -45,11 +71,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.btnClickMe:
                 Toast.makeText(MainActivity.this, "Toast Click", Toast.LENGTH_SHORT).show();
-                textViewWelcomeText.setText(userInput);
-                System.out.println("Priting from interface override");
+//                textViewWelcomeText.setText(userInput);
+                System.out.println("Printing from interface override");
                 break;
             case R.id.editTextName:
                 System.out.println("Writing Text");
+                break;
+            case R.id.leftFirstCheckBox:
+                System.out.println("Clicking Checkbox ####");
                 break;
             default:
                 System.out.println("Printing from default case");
