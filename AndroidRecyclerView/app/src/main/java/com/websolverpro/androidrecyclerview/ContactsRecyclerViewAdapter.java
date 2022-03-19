1868aa230fb1bc909c8fecdb3fd75991dff0769d
mainuldip <mainuldip@gmail.com>
2022-03-19 19:38:01 -0400

    Android RecyclerView Working Initially

--- AndroidRecyclerView/app/src/main/java/com/websolverpro/androidrecyclerview/ContactsRecyclerViewAdapter.java
+++ AndroidRecyclerView/app/src/main/java/com/websolverpro/androidrecyclerview/ContactsRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.websolverpro.androidrecyclerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,28 +8,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
 import androidrecyclerview.R;
 
 public class ContactsRecyclerViewAdapter extends RecyclerView.Adapter<ContactsRecyclerViewAdapter.ViewHolder> {
 
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
     public ContactsRecyclerViewAdapter() {
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
+    /**
+     * Inner class is used here for extending the parent class
+     * also accessing the private TextView txtName from the parent class's onBindViewHolder method to inject data
+     */
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -36,7 +31,45 @@ public class ContactsRecyclerViewAdapter extends RecyclerView.Adapter<ContactsRe
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtName = itemView.findViewById(R.id.contactsRV);
+            txtName = itemView.findViewById(R.id.txtName);
         }
     }
+
+    /**
+     * create the view holder from instantiating the inner class ViewHolder
+     * to create the view we need LayoutInflater from inflate method
+     * RecyclerView and RelativeLayout are all extending from ViewGroup so LayoutInflater.from can get the ViewGroup parent as parent.getContext()
+     * inflate method is self explanatory. finally return the new instance of the innerclass containing the view.
+     */
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contacts_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    /**
+     * This is the most functionally important class for our own ViewHolder Adapter as we bind that here
+     *
+     */
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtName.setText(contacts.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+//        to refresh data inside recyclerview
+        notifyDataSetChanged();
+    }
+
 }
--- AndroidRecyclerView/app/src/main/java/com/websolverpro/androidrecyclerview/MainActivity.java
+++ AndroidRecyclerView/app/src/main/java/com/websolverpro/androidrecyclerview/MainActivity.java
@@ -1,9 +1,13 @@
 package com.websolverpro.androidrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import androidrecyclerview.R;
 
@@ -18,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
 
         contactRV = findViewById(R.id.contactsRV);
 
+        ArrayList<Contact> contacts = new ArrayList<>(Arrays.asList(
+                new Contact("FirstName", "firstname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("SecondName", "secondname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("ThirdName", "thirdname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("FourthName", "thirdname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("FifthName", "thirdname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("SixthName", "thirdname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg"),
+                new Contact("SeventhName", "thirdname@gmail.com", "https://i.pinimg.com/736x/8d/ac/80/8dac805032916139a24513f8ff6c41a6.jpg")
+        ));
+
+        ContactsRecyclerViewAdapter adapter = new ContactsRecyclerViewAdapter();
+
+        adapter.setContacts(contacts);
+
+        contactRV.setAdapter(adapter);
+//
+        contactRV.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
