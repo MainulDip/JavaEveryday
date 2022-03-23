0829e4210ff39a76d16e642ee34842a0666ea2c2
mainuldip <mainuldip@gmail.com>
2022-03-23 02:09:51 -0400

    Upading With Cusotm Font

--- BookManagerAndroid/app/src/main/java/com/websolverpro/bookmanagerandroid/MainActivity.java
+++ BookManagerAndroid/app/src/main/java/com/websolverpro/bookmanagerandroid/MainActivity.java
@@ -3,12 +3,27 @@ package com.websolverpro.bookmanagerandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAllBooks, buttonReading, buttonAlreadyRead, buttonWantToRead, buttonFavourite, buttonAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void initViews(){
+        buttonAllBooks = findViewById(R.id.buttonAllBooks);
+        buttonReading = findViewById(R.id.buttonReading);
+        buttonAlreadyRead = findViewById(R.id.buttonReadAlready);
+        buttonWantToRead = findViewById(R.id.buttonWantToRead);
+        buttonFavourite = findViewById(R.id.buttonFavourite);
+        buttonAbout = findViewById(R.id.buttonAbout);
     }
 }
