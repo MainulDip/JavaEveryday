de8b55ed2295b8904d0ab2d54725798972587f99
mainuldip <mainuldip@gmail.com>
2024-03-06 16:18:05 -0500

    java reflection constructors methods and fields test

--- JavaReflection/JavaReflectionApp/JavaReflectionApp/src/main/java/org/example/animal/Animal.java
+++ JavaReflection/JavaReflectionApp/JavaReflectionApp/src/main/java/org/example/animal/Animal.java
@@ -6,5 +6,17 @@ public abstract class Animal implements Eating {
     public static String CATEGORY = "domestic";
     private String name;
     protected abstract String getSound();
+
+    public static String getCATEGORY() {
+        return CATEGORY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
--- JavaReflection/JavaReflectionApp/JavaReflectionApp/src/test/java/AnimalUnitTest.java
+++ JavaReflection/JavaReflectionApp/JavaReflectionApp/src/test/java/AnimalUnitTest.java
@@ -1,12 +1,33 @@
 import org.example.animal.Goat;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AnimalUnitTest {
+
+    // helper method
+    private static List<String> getFieldNames(Field[] fields){
+        List<String> fieldNames = new ArrayList<>();
+        for (Field field : fields)
+            fieldNames.add(field.getName());
+        return fieldNames;
+    }
+
+    private static List<String> getMethodNames(Method[] methods) {
+        List<String> methodNames = new ArrayList<>();
+        for (Method method : methods)
+            methodNames.add(method.getName());
+        return methodNames;
+    }
 
     // class names
     @Test
@@ -77,5 +98,48 @@ public class AnimalUnitTest {
         assertEquals(1, animalInterfaces.length);
         assertEquals("Locomotion", goatInterfaces[0].getSimpleName());
         assertEquals("Eating", animalInterfaces[0].getSimpleName());
+    }
+
+    // Constructors, Methods and Fields
+    @Test
+    public void givenClass_whenGetsConstructor_thenCorrect() {
+        try {
+            Class<?> goatClass = Class.forName("org.example.animal.Goat");
+            Constructor<?>[] constructor = goatClass.getConstructors();
+
+            assertEquals(1, constructor.length);
+            assertEquals("org.example.animal.Goat", constructor[0].getName());
+            System.out.println("constructor[0].getname() = " + constructor[0].getName());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void givenClass_whenGetsFields_thenCorrect(){
+        try {
+            Class<?> animalClass = Class.forName("org.example.animal.Animal");
+            Field[] fields = animalClass.getDeclaredFields();
+
+            List<String> actualFields = getFieldNames(fields);
+
+            assertEquals(2, actualFields.size());
+//            System.out.println(actualFields.size());
+            assertTrue(actualFields.containsAll(Arrays.asList("name", "CATEGORY")));
+//            actualFields.forEach(System.out::println);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void givenClass_whenGetsMethods_thenCorrect() throws ClassNotFoundException {
+        Class<?> animalClass = Class.forName("org.example.animal.Animal");
+        Method[] methods = animalClass.getDeclaredMethods();
+        List<String> actualMethods = getMethodNames(methods);
+
+        assertEquals(4, actualMethods.size());
+        assertTrue(actualMethods.containsAll(Arrays.asList("getCATEGORY", "getName",
+                "setName", "getSound")));
     }
 }
